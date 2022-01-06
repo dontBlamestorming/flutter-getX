ad77a094012fc7ca92b6297061379105964550f0
dontBlamestorming <lklone2005@gmail.com>
2022-01-06 13:24:59 +0900

    add home ui route

--- lib/constants/app_routes.dart
+++ lib/constants/app_routes.dart
@@ -5,6 +5,9 @@ class AppRoutes {
   AppRoutes._(); // 이 파일 외부로부터 생성자가 호출되는 것을 막는다. named private constructor
 
   static final routes = [
-    GetPage(name: '/', page: () => SplashUI()),
+    GetPage(name: '/', page: () => HomeUI()),
+    GetPage(name: '/signin', page: () => SplashUI()),
+    GetPage(name: '/signup', page: () => SplashUI()),
+    GetPage(name: '/home', page: () => HomeUI()),
   ];
 }
