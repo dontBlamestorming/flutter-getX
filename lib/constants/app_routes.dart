3ec9a74dde6e7665cd9ab7e1cfb6708609b2461c
dontBlamestorming <lklone2005@gmail.com>
2022-01-06 11:47:08 +0900

    add AppRoutes

---
+++ lib/constants/app_routes.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+import 'package:getx/ui/ui.dart';
+
+class AppRoutes {
+  AppRoutes._(); // 이 파일 외부로부터 생성자가 호출되는 것을 막는다. named private constructor
+
+  static final routes = [
+    GetPage(name: '/', page: () => SplashUI()),
+  ];
+}
