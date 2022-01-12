cd38c03fc6b9c56cc8763e4d98feb40b1c4b5bb1
ukdong <lklone20005@gmail.com>
2022-01-12 22:01:40 +0900

    Test commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:getx/constants/constants.dart';
 import 'package:get/get.dart';
 
+// test commit
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
